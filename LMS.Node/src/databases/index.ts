71b2dc8c967c7007dc2328c00e5fcb84ada0b580
Đạt Ngô <ngominhdat110115@gmail.com>
2022-02-17 13:31:40 +0700

    mysql node

--- LMS.Node/src/databases/index.ts
+++ LMS.Node/src/databases/index.ts
@@ -1,7 +1,7 @@
 import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
 
 export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  url: `mongodb+srv://admin:T5ZXaYvBl5a68xVm@cluster0.afkvu.mongodb.net/swagshop?retryWrites=true&w=majority`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
